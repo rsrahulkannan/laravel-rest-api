d19bcf88638eae2cf7e0ba0d02a7f34a93901c94
Rahul R S <rahul.rs@webofficeit.com>
2024-03-14 10:43:47 +0530

    Transforming data into jason

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\CustomerCollection;
+use App\Http\Resources\V1\CustomerResource;
 
 class CustomerController extends Controller
 {
@@ -16,7 +18,8 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        return Customer::all();
+        // return new CustomerCollection(Customer::all()); 
+        return new CustomerCollection(Customer::paginate());
     }
 
     /**
@@ -48,7 +51,7 @@ class CustomerController extends Controller
      */
     public function show(Customer $customer)
     {
-        //
+        return new CustomerResource($customer);
     }
 
     /**
